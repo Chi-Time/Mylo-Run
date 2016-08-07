4fcc2a1efe24021fa19c5d03d55c2493bef84397
[Guardevoir] <music1384@yahoo.co.uk>
2016-08-07 16:03:50 +0100

    Created and implemented platform objects and pool manager.

---
+++ Mylo Run/Assets/Code/Classes/Platform.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform : MonoBehaviour
+{
+	[SerializeField]
+	private float m_Speed = 5.0f;
+	[SerializeField]
+	private float m_CullBoundary = 15.0f;
+
+	private Transform m_Transform = null;
+	private PlatformPool m_Pool = null;
+
+	/// Platform constructor.
+	public void Initialise (PlatformPool pool)
+	{
+		this.gameObject.tag = "Platform";
+		this.gameObject.layer = LayerMask.NameToLayer ("Ground");
+
+		m_Transform = GetComponent <Transform> ();
+		m_Pool = pool;
+	}
+
+	void Update ()
+	{
+		Move ();
+		CheckPosition ();
+	}
+
+	void Move ()
+	{
+		m_Transform.Translate (Vector3.left * m_Speed * Time.deltaTime);
+	}
+
+	void CheckPosition ()
+	{
+		if (m_Transform.position.x < m_CullBoundary)
+			DeActivate ();
+	}
+
+	void DeActivate ()
+	{
+		m_Pool.ReturnToPool (this);
+	}
+}
+
---
+++ Mylo Run/Assets/Code/Classes/PlatformPool.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class PlatformPool
+{
+	[SerializeField]
+	[Tooltip("The total number of objects to spawn in the pool.")]
+	private int m_Amount = 10;
+	[SerializeField]
+	[Tooltip("The default platform to spawn.")]
+	private GameObject[] m_PlatformPrefabs = null;
+	[SerializeField]
+	private List<Platform> m_ActivePlatforms = new List<Platform> ();
+	[SerializeField]
+	private List<Platform> m_InactivePlatforms = new List<Platform> ();
+
+	/// Generates the initial pool elements and ensures they are ready for use.
+	public void GeneratePool ()
+	{
+		var platformHolder = new GameObject ("Platform Holder");
+		platformHolder.transform.SetParent (GameObject.Find ("Controller").transform);
+
+		for(int i = 0; i < m_Amount; i++)
+		{
+			for (int j =0; j < m_PlatformPrefabs.Length; j++)
+			{
+				CreatePlatform (m_PlatformPrefabs[j]);
+			}
+		}
+	}
+
+	void CreatePlatform (GameObject platformToSpawm)
+	{
+		var platform = (GameObject)MonoBehaviour.Instantiate (platformToSpawm, Vector3.zero, Quaternion.identity);
+		platform.transform.SetParent (GameObject.Find ("Platform Holder").transform);
+
+		var c = platform.GetComponent <Platform> ();
+
+		if (c == null)
+			Debug.LogError ("Platform prefab must have platform component attached!");
+		else
+		{
+			c.Initialise (this);
+			AddToPool (platform.GetComponent <Platform> ());
+		}
+	}
+
+	/// Add's the given object to the pool and disables it for later use.
+	void AddToPool (Platform platform)
+	{
+		platform.gameObject.SetActive (false);
+		m_InactivePlatforms.Add (platform);
+	}
+
+	/// Retrieves a platform from the pool and enables it for use.
+	public Platform RetrieveFromPool ()
+	{
+		if(m_InactivePlatforms.Count > 0)
+		{
+			var platform = m_InactivePlatforms [Random.Range (0, m_InactivePlatforms.Count)];
+			platform.gameObject.SetActive (true);
+
+			m_InactivePlatforms.Remove (platform);
+			m_ActivePlatforms.Add (platform);
+
+			return platform;
+		}
+
+		return null;
+	}
+
+	/// Returns the given object back to the bool and disables it for later use.
+	public void ReturnToPool (Platform platform)
+	{
+		platform.gameObject.SetActive (false);
+		m_ActivePlatforms.Remove (platform);
+		m_InactivePlatforms.Add (platform);
+	}
+}
+
