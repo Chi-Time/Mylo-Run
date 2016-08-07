65e9deacf9f096400cb0dca62204cf301d2d9137
[Guardevoir] <music1384@yahoo.co.uk>
2016-08-07 15:14:56 +0100

    Implemented player functionality and created a series of prototype materials and prefabs.

--- Mylo Run/Assets/Code/Classes/Actors/Player.cs
+++ Mylo Run/Assets/Code/Classes/Actors/Player.cs
@@ -1,19 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour
 {
+	[SerializeField]
+	private float m_JumpHeight = 6.0f;
+	[SerializeField]
+	private LayerMask m_GroundLayers;
 
-	// Use this for initialization
-	void Start ()
+	private Rigidbody m_Rigidbody = null;
+
+	void Awake ()
 	{
-	
+		SetupObject ();
 	}
-	
-	// Update is called once per frame
+
+	void SetupObject ()
+	{
+		this.gameObject.tag = "Player";
+
+		m_Rigidbody = GetComponent <Rigidbody> ();
+	}
+
 	void Update ()
 	{
-	
+		CheckInput ();
+	}
+
+	void CheckInput ()
+	{
+		//HACK: Using keycodes. Works for now, but more customisable system should be implemented.
+		if (Input.GetKeyUp (KeyCode.W) || Input.GetKeyUp (KeyCode.UpArrow))
+			Jump ();
+	}
+
+	void Jump ()
+	{
+		if(CanJump ())
+			m_Rigidbody.AddForce (Vector3.up * m_JumpHeight, ForceMode.Impulse);
+	}
+
+	bool CanJump ()
+	{
+		if (Physics.Linecast (transform.position, new Vector3 (transform.position.x, transform.position.y - 0.5f, 0.0f), m_GroundLayers))
+			return true;
+		
+		return false;
 	}
 }
 
