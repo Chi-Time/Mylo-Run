f5317d6166eab59f8a8239176e1c04369c1c114b
[Guardevoir] <music1384@yahoo.co.uk>
2016-08-08 02:36:24 +0100

    Created game controller class to interface and handle all global game values. Implemented basic scoring functionality.

---
+++ Mylo Run/Assets/Code/Classes/Controllers/GameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	public static GameController Instance;
+
+	private int m_Score = 0;
+
+	void Awake ()
+	{
+		if (Instance == null)
+			Instance = this;
+	}
+
+	public void IncreaseScore (int score)
+	{
+		if (PickupController.CurrentPickupState == PickupStates.Doubles)
+			m_Score += score * 2;
+		else
+			m_Score = score;
+	}
+}
+
