47c71a28c9c0e70cbbe002256b2b46a9d9d1e60e
[Guardevoir] <music1384@yahoo.co.uk>
2016-08-08 01:32:59 +0100

    Changed platform controller to make use of new range values. Also refactored parts of the codebase and ensured the platforms cannot go above or below a certain point.

--- Mylo Run/Assets/Code/Classes/Controllers/PlatformController.cs
+++ Mylo Run/Assets/Code/Classes/Controllers/PlatformController.cs
@@ -3,8 +3,27 @@ using System.Collections;
 
 public class PlatformController : MonoBehaviour
 {
+	[Tooltip("The speed at which the background scrolls along the screen.")]
+	public float Speed = 0.0f;
+
 	[SerializeField]
-	private float m_SpawnDelay = 1.0f;
+	[Tooltip("The range of height that a platform will be randomly spawned with.")]
+	private Range m_HeightRange;
+	[SerializeField]
+	[Tooltip("The range of length that a platform will be randomly spawned with.")]
+	private Range m_WidthRange;
+	[SerializeField]
+	[Tooltip("The range of depth that a platform will be randomly spawned with.")]
+	private Range m_DepthRange;
+	[SerializeField]
+	[Tooltip("The random offset between each platform.")]
+	private Range m_OffsetRange;
+	[SerializeField]
+	[Tooltip("The time between each object's spawn.")]
+	private float m_SpawnDelay = 0.0f;
+	[SerializeField]
+	[Tooltip("The boundary at which to de-activate and return an object back to the pool.")]
+	private float m_CullBoundary = 0.0f;
 	[SerializeField]
 	private PlatformPool m_Pool = new PlatformPool ();
 
@@ -13,6 +32,15 @@ public class PlatformController : MonoBehaviour
 	void Awake ()
 	{
 		m_Pool.GeneratePool ();
+		SpawnInitialObjects ();
+	}
+
+	void SpawnInitialObjects ()
+	{
+		for(int i = 0; i < 5; i++)
+		{
+			SpawnPlatform ();
+		}
 	}
 
 	void Start ()
@@ -34,28 +62,50 @@ public class PlatformController : MonoBehaviour
 
 	void ScalePlatform (Platform platform)
 	{
-		platform.transform.localScale = new Vector3 (
-			Random.Range (4f, 8f), 
-			Random.Range (1f, 1.25f), 
-			Random.Range (2f, 4f)
-		);
+		if (m_PreviousPlatform != null)
+		{
+			platform.transform.localScale = new Vector3 (
+				Random.Range (m_WidthRange.Min, m_WidthRange.Max),
+				Random.Range (m_HeightRange.Min, m_HeightRange.Max),
+				Random.Range (m_DepthRange.Min, m_DepthRange.Max)
+			);
+		}
+		else
+			platform.transform.localScale = new Vector3 (8f, 1f, 2f);
+
 	}
 
 	void PositionPlatform (Platform platform)
 	{
 		if (m_PreviousPlatform != null)
 		{
-			float edge = m_PreviousPlatform.transform.position.x + platform.transform.localScale.x;
-			float vertical = m_PreviousPlatform.transform.position.y + platform.transform.localScale.y;
+			float edge = m_PreviousPlatform.transform.position.x + m_PreviousPlatform.transform.localScale.x / 2 + platform.transform.localScale.x / 2;
+			//float vertical = m_PreviousPlatform.transform.position.y + m_PreviousPlatform.transform.localScale.y / 2 + platform.transform.localScale.y / 2;
 
 			platform.transform.position = new Vector3 (
-				edge + Random.Range (2f, 2.5f),
-				vertical + Random.Range (-3f, 2.25f),
+				edge + Random.Range (m_OffsetRange.Min, m_OffsetRange.Max),
+				GetVerticalHeight (),
 				0.0f
 			);
 		}
 		else
-			platform.transform.position = new Vector3 (15f, 0.0f, 0.0f);
+			platform.transform.position = new Vector3 (2.0f, 0.0f, 0.0f);
+	}
+
+	float GetVerticalHeight ()
+	{
+		if(m_PreviousPlatform != null)
+		{
+			if (m_PreviousPlatform.transform.position.y > 200f)
+				return Random.Range (-3f, 0);
+
+			if (m_PreviousPlatform.transform.position.y < -15f)
+				return Random.Range (0f, 3.0f);
+
+			return m_PreviousPlatform.transform.position.y + Random.Range (-3f, 3f);
+		}
+
+		return 0.0f;
 	}
 }
 
